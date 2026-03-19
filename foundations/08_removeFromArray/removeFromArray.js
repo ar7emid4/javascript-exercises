ca332c9e01599b29d0a227626f81b5d47ed64cba
ar7emid4 <134624325+ar7emid4@users.noreply.github.com>
2026-03-19 22:28:14 +0400

    Complete some exercises!

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...unwanted) {
+    return array.filter((item) => !unwanted.includes(item));
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(limit1, limit2) {
+    if (limit1 < 0 
+        || limit2 < 0 
+        || !Number.isInteger(limit1) 
+        || !Number.isInteger(limit2)) return "ERROR"
+    let sum = 0;
+    if (limit1 <= limit2) {
+        for (let i = limit1; i <= limit2; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = limit2; i <= limit1; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
